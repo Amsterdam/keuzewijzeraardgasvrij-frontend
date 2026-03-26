0688121334757e13ef1d75ec42680b843b426779
Remy van der Wereld <r.van.der.wereld@amsterdam.nl>
2026-03-26 15:49:37 +0100

    Add env configuration and update API client to use env variables

---
+++ src/config/env.ts
@@ -0,0 +1,13 @@
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  type MyWindow = Window & { env: any };
+}
+
+type envType = {
+  [name: string]: string | undefined;
+};
+
+export const env: envType = {
+  ...import.meta.env,
+  ...(window as unknown as MyWindow)["env"],
+};
